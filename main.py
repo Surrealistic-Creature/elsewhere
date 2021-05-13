b60ebd03e516fdd7bb227bcfbdd95bf501e40907
surrealistic creature <i.don.t.know.where@gmail.com>
2021-05-13 20:06:44 +0300

    fixes/css/websockets test

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route, Mount
 from starlette.templating import Jinja2Templates
+from jinja2 import Environment, PackageLoader, select_autoescape
 from starlette.staticfiles import StaticFiles
+from starlette.websockets import WebSocket
 
 import database
 
@@ -29,6 +31,11 @@ routes = [
 
 app = Starlette(debug=True, routes=routes, on_startup=[init_db])
 
+async def ws_test(scope, receive, send):
+    websocket = WebSocket(scope=scope, receive=receive, send=send)
+    await websocket.accept()
+    await websocket.send_text('Hello, world!')
+    await websocket.close()
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000, loop='uvloop')
